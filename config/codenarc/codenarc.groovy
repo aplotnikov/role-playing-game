61371e4175d96a39f71e9b93d5160673333aec8f
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-07-02 20:58:15 +0200

    Simplify codenarc configuration

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -1,24 +1,24 @@
 ruleset {
-    ruleset('rulesets/basic.xml')
-    ruleset('rulesets/braces.xml')
-    ruleset('rulesets/concurrency.xml')
-    ruleset('rulesets/convention.xml')
-    ruleset('rulesets/design.xml')
+    ruleset 'rulesets/basic.xml'
+    ruleset 'rulesets/braces.xml'
+    ruleset 'rulesets/concurrency.xml'
+    ruleset 'rulesets/convention.xml'
+    ruleset 'rulesets/design.xml'
     ruleset('rulesets/dry.xml') {
         DuplicateStringLiteral(enabled: false)
         DuplicateNumberLiteral(enabled: false)
     }
-    ruleset('rulesets/enhanced.xml')
+    ruleset 'rulesets/enhanced.xml'
     ruleset('rulesets/formatting.xml') {
         ClassJavadoc(enabled: false)
         SpaceAroundMapEntryColon(enabled: false)
         LineLength(enabled: false)
     }
-    ruleset('rulesets/generic.xml')
-    ruleset('rulesets/groovyism.xml')
-    ruleset('rulesets/exceptions.xml')
-    ruleset('rulesets/imports.xml')
-    ruleset('rulesets/size.xml')
+    ruleset 'rulesets/generic.xml'
+    ruleset 'rulesets/groovyism.xml'
+    ruleset 'rulesets/exceptions.xml'
+    ruleset 'rulesets/imports.xml'
+    ruleset 'rulesets/size.xml'
     ruleset('rulesets/unnecessary.xml') {
         UnnecessaryBooleanExpression(enabled: false) // looks like a bug with parametrized tests
         UnnecessaryGetter(enabled: false) // looks like a bug with spock closures
